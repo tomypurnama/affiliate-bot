ddbe792b116c04621c39f20711035d4bcdb42046
tomypurnama <tomypurnamaputra@gmail.com>
2026-03-28 10:26:43 +0700

    Update affiliate_cmd.py

--- handlers/affiliate_cmd.py
+++ handlers/affiliate_cmd.py
@@ -1,6 +1,13 @@
-from modules.ai_generator import generate_content
+from config import USE_AI
 from modules.affiliate import get_affiliate_link
 from modules.formatter import format_message
+
+# pilih generator berdasarkan mode
+if USE_AI:
+    from modules.ai_generator import generate_content
+else:
+    from modules.dummy_generator import generate_content
+
 
 def handle_affiliate(keyword):
     content = generate_content(keyword)
